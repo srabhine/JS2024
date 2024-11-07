d1bcd4a80b20666e8d974b017443e6711a36c321
deckard <rghigliaus@yahoo.com>
2024-11-07 10:32:45 -0500

    Added Google drives url's.

--- libs/io_lib/paths.py
+++ libs/io_lib/paths.py
@@ -1,4 +1,9 @@
 
+# Google drive:
+# https://drive.google.com/file/d/1a3NX9wsXCBKscJO43WUEPHuxGbIP40Dh
+# https://drive.google.com/drive/folders/135BWdBY-d0fp7jYqKxoaToLVS5g3rGV5
+
+
 # Paths
 import os
 from pathlib import Path
