497bcef73a241bac7439c1e3331511fe3b297199
deckard <rghigliaus@yahoo.com>
2024-11-19 10:23:14 -0500

    Trying to load data.

--- scripts/Raf/check_data.py
+++ scripts/Raf/check_data.py
@@ -2,6 +2,7 @@
 
 @author: Raffaele M Ghigliazza
 """
+<<<<<<< HEAD
 import polars as pl
 from data_lib.datasets import load_data_by_dates
 from io_lib.paths import LAGS_FEATURES_TRAINING
@@ -9,3 +10,8 @@ from io_lib.paths import LAGS_FEATURES_TRAINING
 # data = load_data_by_dates(dates=list(range(1654, 1658)))
 
 df = pl.scan_parquet(LAGS_FEATURES_TRAINING).collect().to_pandas()
+=======
+from data_lib.datasets import load_data_by_dates
+
+data = load_data_by_dates(dates=list(range(1654, 1658)))
+>>>>>>> origin/main
