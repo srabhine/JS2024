93fcc656e7296f7e68446db6f41b405a036d935d
deckard <rghigliaus@yahoo.com>
2024-11-20 11:05:48 -0500

    Added comment.

--- scripts/Raf/load_model_ex.py
+++ scripts/Raf/load_model_ex.py
@@ -110,3 +110,12 @@ f, ax = plt.subplots(1)
 ax.plot(predictions)
 plt.show()
 
+
+
+# Seed 1234
+# tanh: R² Score on validation data: 0.022405683994293213
+# linear: R² Score on validation data: 0.014503955841064453
+
+# Seed 0
+# tanh: R² Score on validation data: -0.06566083431243896
+# linear: R² Score on validation data: 0.014717578887939453
