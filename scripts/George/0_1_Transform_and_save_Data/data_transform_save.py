36cbd3518116ac1a4d231c4f3f8f41e86dd5ea50
zt <zhongtianye@hotmail.com>
2024-11-25 19:34:12 +0800

    George update

--- scripts/George/0_1_Transform_and_save_Data/data_transform_save.py
+++ scripts/George/0_1_Transform_and_save_Data/data_transform_save.py
@@ -13,12 +13,12 @@ from features_lib.core import transform_features
 # case = 'feats_time_lag'
 # case = 'resp_day_lag'
 # feature_names = FEATS + FEATS_TIME_LAG + RESP_DAY_LAG
-# feature_names = FEATS
+feature_names = FEATS
 
 
 # sym = 2
-# sym = SYMBOLS
-sym = [3, 4]
+sym = SYMBOLS
+# sym = [3, 4]
 out_layer = 'tanh'
 # out_layer = 'linear'
 
@@ -36,12 +36,14 @@ feat_types_dic = get_features_classification()
 # path = "E:\Python_Projects\JS2024\GITHUB_C\data\\non_lag_data\\training_parquet" \
 #        "\\date_id=1501\\*.parquet"
 
-for i in range(0,10):
-    path ="E:\Python_Projects\Optiver\JaneStreetMktPred\data\jane-street-real-time-market-data-forecasting\\train.parquet\partition_id=8"
+for i in range(2,10):
+    path =f"/home/zt/pyProjects/Optiver/JaneStreetMktPred/data/jane-street-real-time-market-data-forecasting/train.parquet/partition_id={i}"
+    path = f"/home/zt/pyProjects/Optiver/JaneStreetMktPred/data/jane-street-real-time-market-data-forecasting/train.parquet/partition_id=7"
     df = pl.scan_parquet(path).collect().to_pandas()
     
-    df = transform_features(df, feat_types_dic)
+    df = transform_features(df, feat_types_dic)[0]
     df = pl.DataFrame(df)
+
     df.write_parquet(
-        f"E:\Python_Projects\JS2024\GITHUB_C\data\\transformed_data\\train_trans", partition_by = "date_id",
+        "/home/zt/pyProjects/JaneSt/Team/data/transformed_data", partition_by="date_id",
     )
