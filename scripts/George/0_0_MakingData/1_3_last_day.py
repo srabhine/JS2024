308542f65c9894533aebb5e934ae749d86d0fc05
zt <zhongtianye@hotmail.com>
2024-11-26 22:24:56 +0800

    George update base use transformed data and a trained model

---
+++ scripts/George/0_0_MakingData/1_3_last_day.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import polars as pl
+import numpy as np
+
+start_date = 0
+last_train_date = 1600
+feature_names = (["symbol_id", "date_id", "time_id", "weight"] +[f"feature_{i:02d}" for i in range(79)] + [f"responder_6"])
+
+
+def get_all_data(start_date):
+    data = pl.scan_parquet(
+        f"/home/zt/pyProjects/Optiver/JaneStreetMktPred/data/jane-street-real-time-market-data-forecasting/train.parquet"
+    ).select(
+        pl.int_range(pl.len(), dtype=pl.UInt32).alias("id"),
+        pl.all(),
+    ).filter(
+        pl.col("date_id").gt(start_date)
+    )
+    return data # lazy dataframe
+
+def get_last_day_data(data):
+    data = data.select(pl.col(feature_names))
+    data = data.group_by(["symbol_id", "date_id"], maintain_order=True).last()  # pick up last record of previous date
+    return data
+
+
+def get_mean_data(data):
+    data = data.select(pl.col(feature_names))
+    agg_columns = [pl.mean(col).alias(f"mean_{col}") for col in feature_names if col not in ["symbol_id", "date_id", "time_id"]]
+    data = data.group_by(["symbol_id", "date_id"]).agg(agg_columns)
+    return data
+
+
+
+data = get_all_data(start_date)
+
+data = get_mean_data(data)
+
+
+data.collect().write_parquet(
+    f"/home/zt/pyProjects/JaneSt/Team/data/mean_data/train.parquet"
+)
+
---
+++ scripts/George/1_0_NN_PlainVanilla/TF_NN_Base_MeanData.py
@@ -0,0 +1,110 @@
+import os
+import polars as pl
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from tensorflow.keras import layers, models, optimizers, regularizers, callbacks
+from tensorflow.keras.layers import Input, Dense, Concatenate
+from tensorflow.keras.models import Model
+
+
+def create_model(input_dim, lr, weight_decay):
+    # Create a sequential model
+    model = models.Sequential()
+
+    model.add(layers.Input(shape=(input_dim,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Activation('swish'))
+    model.add(layers.Dropout(0.1))  # Assuming dropouts[1] is valid
+    model.add(layers.Dense(128))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Activation('swish'))
+    model.add(layers.Dropout(0.1))  # Assuming dropouts[1] is valid
+    model.add(layers.Dense(64, kernel_regularizer=regularizers.l2(weight_decay)))
+    # Output layer
+    model.add(layers.Dense(1, activation='tanh'))
+
+    # Compile model with Mean Squared Error loss
+    # model.compile(optimizer=optimizers.Adam(learning_rate=lr), loss='mse', metrics=[WeightedR2()])
+    model.compile(optimizer=optimizers.Adam(learning_rate=lr),
+                  loss='mse',
+                  metrics=[tf.keras.metrics.R2Score(class_aggregation='uniform_average')])
+    return model
+
+
+
+
+data_path = "/home/zt/pyProjects/JaneSt/Team/data/mean_data/train.parquet"
+model_saving_path = "/home/zt/pyProjects/JaneSt/Team/scripts/George/models/3_base_model_mean_data"
+model_saving_name = "model_3_Base_mean_data_{epoch:02d}.keras"
+
+feature_names = [f"mean_feature_{i:02d}" for i in range(79)]
+label_name = 'mean_responder_6'
+weight_name = 'weight'
+
+data_train = pl.scan_parquet(data_path).select(
+    pl.int_range(pl.len(), dtype=pl.UInt32).alias("id"),
+    pl.all(),).filter(
+    pl.col("date_id").le(1500))
+data_train = data_train.collect().to_pandas()
+data_train = data_train.fillna(0)
+
+data_valid = pl.scan_parquet(data_path).select(
+    pl.int_range(pl.len(), dtype=pl.UInt32).alias("id"),
+    pl.all(),).filter(
+    pl.col("date_id").gt(1501))
+data_valid = data_valid.collect().to_pandas()
+data_valid = data_valid.fillna(0)
+
+
+
+X_train = data_train[ feature_names ]
+y_train = data_train[ label_name    ]
+w_train = data_train[ "mean_weight" ]
+X_valid = data_valid[ feature_names ]
+y_valid = data_valid[ label_name    ]
+w_valid = data_valid[ "mean_weight" ]
+
+del data_train
+del data_valid
+
+
+
+
+lr = 0.01
+weight_decay = 1e-6
+input_dimensions = X_train.shape[1]
+model = create_model(input_dimensions, lr, weight_decay)
+
+
+ca = [
+    tf.keras.callbacks.EarlyStopping(monitor='val_r2_score', patience=25, mode='max'),
+    tf.keras.callbacks.ModelCheckpoint(
+        filepath=f'{model_saving_path}/{model_saving_name}',
+        monitor='val_loss', save_best_only=False),
+    tf.keras.callbacks.ReduceLROnPlateau(
+        monitor='val_loss',  # Metric to be monitored
+        factor=0.1,  # Factor by which the learning rate will be reduced
+        patience=10,  # Number of epochs with no improvement after which learning rate will be reduced
+        verbose=1,  # Verbosity mode
+        min_lr=1e-6  # Lower bound on the learning rate
+    )
+
+]
+
+
+
+model.fit(
+    x=X_train,  # Input features for training
+    y=y_train,                          # Target labels for training
+    sample_weight=w_train,              # Sample weights for training
+    validation_data=(X_valid, y_valid, w_valid),  # Validation data
+    batch_size=8092,                      # Batch size
+    epochs=100,                        # Number of epochs
+    callbacks=ca,                # Callbacks list, if any
+    verbose=1,                           # Verbose output during training
+    shuffle=True
+)
+
