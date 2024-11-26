826a0eac72e2d303f53fbcb09487b5c64fcfd59e
deckard <rghigliaus@yahoo.com>
2024-11-26 09:16:26 -0500

    Completed code for cleanup, normalization, and transformation.

--- libs/one_big_lib.py
+++ libs/one_big_lib.py
@@ -7,8 +7,6 @@ from typing import Optional, List, Union, Dict
 
 import numpy as np
 import pandas as pd
-import polars as pl
-
 
 SYMBOLS = list(range(39))
 RESPONDERS = list(range(9))
@@ -138,15 +136,103 @@ def calc_scalers(
     return scalers_mu, scalers_sg
 
 
+def transform_data(
+        data_all: pd.DataFrame,
+        feature_names: Optional[List] = None,
+        transformation: Optional[Union[str,
+        Dict[str, str]]] = None):
+    data_by_sym = stack_features_by_sym(data_all,
+                                        feature_names=feature_names)
+    # (date_id, time_id) x (features+, 'symbol_id')
+    transf_dic = get_transformation_dic(data_all,
+                                        transformation=transformation)
+    feat_names = data_by_sym.columns.droplevel(1).unique()
+    symbol_ids = data_by_sym.columns.droplevel(0).unique()
+    params = {}
+    data_transformed = data_by_sym.copy()
+    for f in feat_names:
+        params[f] = {}
+        if f in transf_dic:
+            if transf_dic[f] in ['norm', 'normal']:
+                params[f] = {'loc': data_by_sym[f].mean(),
+                             'scale': data_by_sym[f].std()}
+                for s in symbol_ids:
+                    data_transformed[(f, s)] = \
+                        ((data_by_sym[(f, s)] - params[f]['loc'][s])
+                         / params[f]['scale'][s])
+            elif transf_dic[f] == 'log':
+                params[f] = {'shift': pd.Series(index=symbol_ids)}
+                for s in symbol_ids:
+                    ds = data_by_sym[(f, s)]
+                    a = 1.1 * ds.min() if ds.min() < 0 else 0.9
+                    params[f]['shift'].loc[s] = a
+                    data_transformed[(f, s)] = \
+                        np.log(data_by_sym[(f, s)] - a)
+            elif transf_dic[f] == '0_1':
+                params[f] = {'min': data_by_sym[f].min(),
+                             'max': data_by_sym[f].max()}
+                for s in symbol_ids:
+                    ds = data_by_sym[(f, s)]
+                    mn = params[f]['min'].loc[s]
+                    mx = params[f]['max'].loc[s]
+                    data_transformed[(f, s)] = (ds - mn) / (mx - mn)
+            elif transf_dic[f] == '-1_1':
+                params[f] = {'min': data_by_sym[f].min(),
+                             'max': data_by_sym[f].max()}
+                for s in symbol_ids:
+                    ds = data_by_sym[(f, s)]
+                    mn = params[f]['min'].loc[s]
+                    mx = params[f]['max'].loc[s]
+                    data_transformed[(f, s)] = \
+                        2 * (ds - mn) / (mx - mn) - 1
+            elif transf_dic[f] == 'tanh':
+                params[f] = {}
+                for s in symbol_ids:
+                    data_transformed[(f, s)] = \
+                        np.tanh(data_by_sym[(f, s)])
+    data_transformed = unstack_features_by_sym(
+        data_transformed)
+    return data_transformed, params
+
+
 if __name__ == '__main__':
+    import polars as pl
+    from matplotlib import pyplot as plt
     from data_lib.datasets import get_features_classification
     from io_lib.paths import LAGS_FEATURES_TRAIN
 
-    feat_types_dic = get_features_classification()
     data_tmp = pl.scan_parquet(
         LAGS_FEATURES_TRAIN).collect().to_pandas()
+
+    # Cleanup
+    data_by_sym = stack_features_by_sym(data_tmp)
+    data_cleaned = unstack_features_by_sym(data_by_sym)
+    print(data_tmp.shape, data_cleaned.shape)
+
+    # Normalization
     s_mu, s_sg = calc_scalers(data_tmp, transformation='norm')
     s_mu, s_sg = calc_scalers(data_tmp,
                               transformation={'feature_71': 'norm',
                                               'responder_6': 'none'},
-                              )
+                                  )
+
+    # Transformation
+    feat_types_dic = get_features_classification()
+    feat_types_dic['responder_6'] = '0_1'
+    feat_types_dic['responder_6'] = '-1_1'
+    feat_types_dic['responder_6'] = 'tanh'
+    data_transf, params = transform_data(
+        data_tmp, transformation=feat_types_dic)
+
+    print(params)
+
+    data_org = data_tmp.set_index(['date_id', 'time_id',
+                                           'symbol_id'])
+    data_t = data_transf.set_index(['date_id', 'time_id',
+                                           'symbol_id'])
+    data_plot = pd.concat((data_org[TARGET], data_t[TARGET]),
+                          axis=1, keys=['raw', 'transf']).dropna()
+
+    f, ax = plt.subplots()
+    ax.scatter(data_plot.iloc[:, 0], data_plot.iloc[:, 1])
+    plt.show()
