aed2b72581b9e70c4b67f7794f6766b66134a794
deckard <rghigliaus@yahoo.com>
2024-11-28 12:49:30 -0500

    One hot encoding.

--- scripts/Raf/thought_on_sign.py
+++ scripts/Raf/thought_on_sign.py
@@ -26,3 +26,22 @@ s_rnd = np.sign(np.random.randn(len(signs))).mean()
 sum(abs(signs.mean()) > s_rnd)
 
 
+y = df['responder_6']
+y_s = np.sign(y)
+
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
+X_num = df.select_dtypes(exclude='object')
+X_cat = df.select_dtypes(include='object')
+encoder = OneHotEncoder(handle_unknown='error')
+X_encoded = encoder.fit_transform(y_s.to_frame())
+print(X_encoded)
+
+classes = [-1, 0, 1]
+enc = LabelEncoder()
+encoder.fit(np.array(classes).reshape(-1, 1))
+
+X_encoded = encoder.transform(y_s.values.reshape(-1, 1))
+
+a = X_encoded.todense()
+print(a)
