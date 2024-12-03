764e2537ddd4c7e0c5e79bee700ac2696b406ca1
Zoutain <65902088+Zoutain@users.noreply.github.com>
2024-12-03 16:59:24 +0800

    george updated lgbm

--- scripts/George/8_0_ML_XGB/8_0_1_base_XGB.py
+++ scripts/George/8_0_ML_XGB/8_0_1_base_XGB.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import polars as pl
 import numpy as np
-import joblib
 import os
 from sklearn.metrics import r2_score
 import random
 import pickle
-from lightgbm import LGBMRegressor
-from lightgbm import LGBMRegressor, log_evaluation, early_stopping
+from xgboost import XGBRegressor
+
 def set_random_seeds(seed=42):
     # Set the random seed for reproducibility
     np.random.seed(seed)
@@ -34,31 +33,6 @@ def load_data(path, start_dt, end_dt):
     return data
 
 
-def ScoreMetric(ytrue, ypred, weight):
-    """
-    This function is a modification of the ready-made R-square function with sample weight.
-    We have this as a column in the dataset
-    """
-
-    return r2_score(ytrue, ypred, sample_weight=weight)
-
-
-class CustomMetricMaker:
-    "This class makes the custom metric for LGBM and XGBoost early stopping"
-
-    def __init__(self, method):
-        self.method = method
-
-    def make_metric(self, ytrue, ypred, weight):
-        """
-        This method returns the relevant metric for LGBM and XGB.
-        Catboost has a slightly different signature for the same- will be provided in version 2
-        """
-
-        if "LGB" in self.method:
-            return 'Wgt_RSquare', ScoreMetric(ytrue, ypred, weight), True
-        else:
-            return ScoreMetric(ytrue, ypred, weight)
 
 
 is_linux = False
@@ -87,45 +61,58 @@ col_to_train = feature_names
 
 
 # X_train = data_train[feature_names]
-X_train = load_data(path, start_dt=1200, end_dt=1500)
+X_train = load_data(path, start_dt=1450, end_dt=1500)
 y_train = X_train[label_name]
 w_train = X_train["weight"]
 X_train = X_train[col_to_train]
 
-X_valid = load_data(path, start_dt=1501, end_dt=1690)
+X_valid = load_data(path, start_dt=1650, end_dt=1690)
 y_valid = X_valid[label_name]
 w_valid = X_valid["weight"]
 X_valid = X_valid[col_to_train]
 
 
 # Define custom R² calculation
-def r2_val(y_true, y_pred, sample_weight):
-    """Calculate weighted R² value."""
-    numerator = np.average((y_pred - y_true) ** 2, weights=sample_weight)
-    denominator = np.average(y_true ** 2, weights=sample_weight) + 1e-38
-    r2 = 1 - numerator / denominator
-    return r2
+def r2_xgb(y_true, y_pred, sample_weight=None):
+    if sample_weight is None:
+        sample_weight = np.ones_like(y_true)
+    r2 = 1 - np.average((y_pred - y_true) ** 2, weights=sample_weight) / (np.average((y_true) ** 2, weights=sample_weight) + 1e-38)
+    return -r2
 
-model = LGBMRegressor(
-    device="gpu",
-    objective="regression_l2",
-    boosting_type = 'gbdt',
-    n_estimators=1500,
-    max_depth=11,
-    learning_rate=0.01,
-    colsample_bytree=0.6,
-    subsample=0.6,
-    random_state=42,
-    reg_lambda=0.8,
-    reg_alpha=0.1,
-    num_leaves=800,
-    verbosity=-1
+def get_model(seed):
+    # XGBoost parameters
+    XGB_Params = {
+        'booster':          'gbtree',
+        'learning_rate':        0.01,
+        'n_estimators':         2000,
+        'early_stopping_rounds': 100,
+        'enable_categorical':   True,
+        'grow_policy':   'lossguide',
+        'max_cat_to_onehot':       4,
+        'max_depth':              14,
+        'subsample':             0.6,
+        'colsample_bytree':      0.7,
+        'reg_alpha':               2,
+        'reg_lambda':              8,
+        'random_state':         seed,
+        'tree_method':        'hist',
+        'device':             'cuda',
+        'n_gpus':                  1,
+        'eval_metric':        r2_xgb,
+        'verbosity':               1
+    }
+    XGB_Model = XGBRegressor(**XGB_Params)
+    return XGB_Model
+
+
+model = get_model(42)
+model.fit(
+    X_train, y_train,
+    sample_weight=w_train,
+    eval_set=[(X_valid, y_valid)],
+    sample_weight_eval_set=[w_valid],
+    verbose=10  # print logs every 100 rounds
 )
-
-mymetric = CustomMetricMaker(method="LGB")
-model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)],
-    eval_names=["valid"], eval_metric=[mymetric.make_metric], sample_weight=w_train,
-    callbacks=[log_evaluation(10),early_stopping(100, verbose=True)])
 
 
 
@@ -135,6 +122,6 @@ valid_score = r2_score(y_valid, y_pred_valid, sample_weight=w_valid)
 print(f"Validation R² Score: {valid_score}")
 
 
-with open("E:\Python_Projects\JS2024\GITHUB_C\scripts\George\\5_0_ML_LGBM\models\\5_0_1_base\\5_0_1_ML_base_lgbm.pkl", 'wb') as model_file:
+with open("E:\Python_Projects\JS2024\GITHUB_C\scripts\George\8_0_ML_XGB\models\\8_0_1_ML_base_xgb.pkl", 'wb') as model_file:
     pickle.dump(model, model_file)
 
